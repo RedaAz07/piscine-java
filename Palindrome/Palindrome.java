720027ccfdb48984b13560137bc29e10e5a2feeb
ranniz <annizreda07@gmail.com>
2026-02-22 15:09:58 +0000

    ss

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,9 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+    if (s  == null){
+        return false ;
+    }
+
         StringBuilder k =   new StringBuilder(s) ; 
         k.reverse();
         return  s.toLowerCase().equals(k.toString().toLowerCase()) ;
