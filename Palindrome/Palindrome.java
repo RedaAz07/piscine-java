5aa48f9d315497274f479a2d33ced60a3ad90ade
ranniz <annizreda07@gmail.com>
2026-02-22 15:08:22 +0000

    fix blind

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -2,7 +2,7 @@ public class Palindrome {
     public static boolean isPalindrome(String s) {
         StringBuilder k =   new StringBuilder(s) ; 
         k.reverse();
-        return  s.equals(k.toString().toLowerCase()) ;
+        return  s.toLowerCase().equals(k.toString().toLowerCase()) ;
 
     }
 }
