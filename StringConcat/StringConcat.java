a5f9ed36699d300547629e4a7bf6b1c0841eea12
ranniz <annizreda07@gmail.com>
2026-02-22 12:02:54 +0000

    ss

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,6 +1,9 @@
 import java.util.Objects;
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return  null;
+        }
         return Objects.toString(s1,"") + Objects.toString(s2,"");
     }
 }
--- StringConcat/run.java
+++ StringConcat/run.java
@@ -1,6 +1,6 @@
 public class run {
     public static void main(String[] args) {
-        System.out.println(StringConcat.concat(null, "étudiant !"));
+        System.out.println(StringConcat.concat(null, null));
         System.out.println(StringConcat.concat("", "Hello World !"));
     }
 }
