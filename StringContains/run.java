e0f50c10b5eff263fb2d43edc97f9283f7371f76
ranniz <annizreda07@gmail.com>
2026-02-22 12:13:56 +0000

    contanes

---
+++ StringContains/run.java
@@ -0,0 +1,7 @@
+public class run {
+    public static void main(String[] args) {
+        System.out.println(StringContains.isStringContainedIn(null, "Highway to Hell"));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
+    }
+}
