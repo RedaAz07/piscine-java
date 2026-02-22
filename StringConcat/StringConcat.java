0dd6091171bd0c35edc52f088cd8a47d49b44cbe
ranniz <annizreda07@gmail.com>
2026-02-22 11:59:33 +0000

    string concate

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,6 @@
+import java.util.Objects;
 public class StringConcat {
     public static String concat(String s1, String s2) {
-      return  s1 +s2 ;
+        return Objects.toString(s1,"") + Objects.toString(s2,"");
     }
 }
