0f7e9d425f65246431f3302dedcf59adb336e72b
ranniz <annizreda07@gmail.com>
2026-02-22 11:44:38 +0000

    StringLength

---
+++ StringLength/run.java
@@ -0,0 +1,6 @@
+public class run {
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello World !"));
+        System.out.println(StringLength.getStringLength(""));
+    }
+}
