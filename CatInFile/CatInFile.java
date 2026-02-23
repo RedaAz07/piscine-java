c00e54a0d76271a06dba292fe1512377566984c2
RedaAz07 <annizreda07@gmail.com>
2026-02-23 12:35:54 +0000

    opuhs

---
+++ CatInFile/CatInFile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CatInFile {
+    public static void cat(String[] args) throws IOException {
+     System.in.transferTo(
+        Files.newOutputStream(Paths.get(args[0]))
+    );
+    }
+
+    public static void main(String[] args) throws IOException {
+        String input = "input file test\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        CatInFile.cat(new String[] { "output" });
+        String fileContent = new String(Files.readAllBytes(Paths.get("output")));
+        System.out.println(fileContent.equals(input));
+    }
+}
