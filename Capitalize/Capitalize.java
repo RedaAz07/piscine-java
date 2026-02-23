afc569e96a00318292b157ad6401a1a358fee319
RedaAz07 <annizreda07@gmail.com>
2026-02-23 13:32:57 +0000

    cap

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Capitalize {
+    public static void capitalize(String[] args) throws IOException {
+        if (args.length != 2) {
+            return;
+        }
+        File input = new File(args[0]);
+
+        FileWriter myWriter = new FileWriter(args[1]);
+        try (Scanner myReader = new Scanner(input)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] arr = data.split("\\ ");
+                StringBuilder result = new StringBuilder();
+                for (String el : arr) {
+                    if (el.length() == 0) {
+                        return;
+                    }
+                    String ch = Character.toString(el.charAt(0));
+                    String sub = el.substring(1);
+                    String res = ch.toUpperCase() + sub.toLowerCase();
+                    result.append(res);
+                }
+                myWriter.write(result.toString());
+                myWriter.close();
+
+            }
+        } catch (FileNotFoundException e) {
+            return;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Capitalize.capitalize(new String[] { "input", "output" });
+        String expectedResult = new String(Files.readAllBytes(Paths.get("result")));
+        String userOutput = new String(Files.readAllBytes(Paths.get("output")));
+        System.out.println(expectedResult.equals(userOutput));
+    }
+}
