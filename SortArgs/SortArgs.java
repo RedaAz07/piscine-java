21e0d8c8bdbaa98bdd3f768d0876b83b57e7ca85
RedaAz07 <annizreda07@gmail.com>
2026-02-23 11:26:23 +0000

    sort args

---
+++ SortArgs/SortArgs.java
@@ -0,0 +1,25 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class SortArgs {
+    public static void sort(String[] args) {
+        Arrays.sort(args);
+        System.out.println(String.join(" ", args));
+    }
+
+    public static void main(String[] args) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        var defaultOut = System.out;
+
+        System.setOut(printStream);
+        SortArgs.sort(new String[] { "4", "2", "1", "3" });
+        System.setOut(defaultOut);
+
+        String output = outputStream.toString();
+        System.out.println(output.equals("1 2 3 4\n"));
+    }
+}
