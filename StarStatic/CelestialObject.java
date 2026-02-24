3dbb4feb1de228da42423d3210a45e4e90c9d978
RedaAz07 <annizreda07@gmail.com>
2026-02-24 11:43:27 +0000

    StarStatic

---
+++ StarStatic/CelestialObject.java
@@ -0,0 +1,83 @@
+
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    public static final double KM_IN_ONE_AU = 150000000;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static double getDistanceBetween(CelestialObject first, CelestialObject end) {
+        double dx = end.x - end.x;
+        double dy = end.y - end.y;
+        double dz = end.z - end.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject first, CelestialObject end) {
+
+        return getDistanceBetween(first, end) * KM_IN_ONE_AU;
+    }
+
+    public static void main(String[] args) {
+        CelestialObject defaultStar = new CelestialObject();
+        System.out.println(defaultStar.getX());
+        System.out.println(defaultStar.getY());
+        System.out.println(defaultStar.getZ());
+        System.out.println(defaultStar.getName());
+
+        defaultStar.setName("Terre");
+        defaultStar.setX(0.43);
+        defaultStar.setY(0.98);
+        defaultStar.setZ(1.43);
+        System.out.println(defaultStar.getX());
+        System.out.println(defaultStar.getY());
+        System.out.println(defaultStar.getZ());
+        System.out.println(defaultStar.getName());
+    }
+}
