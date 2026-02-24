2c278c55a2afd06836e03fd9e746a91de5f51636
RedaAz07 <annizreda07@gmail.com>
2026-02-24 12:23:32 +0000

    StarUtils

---
+++ StarUtils/CelestialObject.java
@@ -0,0 +1,88 @@
+
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    public static final double KM_IN_ONE_AU = 150000000;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static double getDistanceBetween(CelestialObject first, CelestialObject end) {
+        double dx = end.x - first.x;
+        double dy = end.y - first.y;
+        double dz = end.z - first.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject first, CelestialObject end) {
+        return getDistanceBetween(first, end) * KM_IN_ONE_AU;
+    }
+
+    public String toString() {
+        return String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, this.x, this.y, this.z);
+    }
+
+    public boolean equals(CelestialObject object) {
+     
+        return object.name.equals(this.name) && object.x == this.x && object.y == this.y && object.z == this.z;
+
+    }
+
+     public static void main(String[] args) {
+        CelestialObject celestialObject = new CelestialObject();
+        CelestialObject earth = new CelestialObject("Terre", 1.0, 2.0, 2.0);
+        CelestialObject earth1 = new CelestialObject("Terre", 1.0, 2.0, 2.0);
+
+        System.out.println(earth.toString());
+        System.out.println(earth.equals(earth1));
+        System.out.println(earth.equals(celestialObject));
+
+        System.out.println(earth.hashCode());
+        System.out.println(celestialObject.hashCode());
+    }
+}
