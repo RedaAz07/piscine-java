ce17818723fd259d5258f81f46db937ecfb20ed1
RedaAz07 <annizreda07@gmail.com>
2026-02-24 15:09:18 +0000

    splanet

---
+++ StarPlanet/CelestialObject.java
@@ -0,0 +1,90 @@
+import java.util.Objects;
+
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    public static final double KM_IN_ONE_AU = 150000000;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static double getDistanceBetween(CelestialObject first, CelestialObject end) {
+        double dx = end.x - first.x;
+        double dy = end.y - first.y;
+        double dz = end.z - first.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject first, CelestialObject end) {
+        return getDistanceBetween(first, end) * KM_IN_ONE_AU;
+    }
+
+    public String toString() {
+        return String.format("%s is positioned at (%.3f, %.3f, %.3f)", this.name, this.x, this.y, this.z);
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        CelestialObject that = (CelestialObject) obj;
+        return Double.compare(that.x, this.x) == 0 &&
+                Double.compare(that.y, this.y) == 0 &&
+                Double.compare(that.z, this.z) == 0 &&
+                Objects.equals(this.name, that.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, x, y, z);
+    }
+
+ 
+}
