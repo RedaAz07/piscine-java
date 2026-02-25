5ca32044cc20b89c7d725f39c432fd78c7880981
RedaAz07 <annizreda07@gmail.com>
2026-02-25 12:10:05 +0000

    d

--- StarMass/Galaxy.java
+++ StarMass/Galaxy.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Galaxy {
-    private final List<CelestialObject> celestialObjects;
+    private  List<CelestialObject> celestialObjects;
 
     public Galaxy() {
         this.celestialObjects = new ArrayList<>();
