9cec01f49182438b03aa089930e952184e572bb2
RedaAz07 <annizreda07@gmail.com>
2026-03-01 11:23:08 +0000

    AdventureCharacter

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,64 @@
+package AdventureCharacter;
+
+public class Character {
+
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+
+    public int getMaxHealth() {
+        return this.maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return this.currentHealth;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Character(String name, int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.name = name;
+        this.currentHealth=maxHealth;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth == 0) {
+            return String.format("%s :KO", this.name);
+        } else {
+            return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+        }
+
+    }
+
+    public void takeDamage(int nb) {
+        if (this.currentHealth - nb >= 0) {
+            this.currentHealth -= nb;
+        } else {
+            this.currentHealth = 0;
+        }
+
+    }
+
+    public void attack(Character chart) {
+        chart.takeDamage(9);
+    }
+
+    public static void main(String[] args) {
+        Character aragorn = new Character("Aragorn", 20);
+        Character uruk = new Character("Uruk", 5);
+        System.out.println(aragorn.toString());
+        System.out.println(uruk.toString());
+
+        aragorn.attack(uruk);
+
+        System.out.println(uruk.toString());
+
+        aragorn.takeDamage(12);
+
+        System.out.println(aragorn.toString());
+    }
+}
