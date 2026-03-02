1c6c5410e997ef79dbe810526b8067b19d896cc8
RedaAz07 <annizreda07@gmail.com>
2026-03-02 11:52:52 +0000

    exception

---
+++ AdventureException/Monster.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Monster extends Character {
+    public Monster(String name, int ma, Weapon w) {
+        super(name, ma,w);
+    }
+
+      @Override
+    public void attack(Character chart) {
+        if (Objects.nonNull(this.getWeapon())) {
+            chart.takeDamage(this.getWeapon().getDamage());
+        } else {
+            chart.takeDamage(7);
+        }
+    }
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() > 0) {
+            return String.format("%s is a monster with %d HP. He has the weapon %s", this.getName(), this.getCurrentHealth(),this.getWeapon().toString());
+        } else {
+            return String.format("%s is a monster and is dead. He has the weapon %s", this.getName(), this.getWeapon().toString());
+
+        }
+
+    }
+
+    @Override
+    public void takeDamage(int nb) {
+        if (this.getCurrentHealth() - (int) (nb * 0.8) >= 0) {
+            this.setCurrentHealth(this.getCurrentHealth() - (int) (nb * 0.8));
+        } else {
+            this.setCurrentHealth(0);
+        }
+
+    }
+
+
+}
---
+++ AdventureException/Templar.java
@@ -0,0 +1,67 @@
+
+import java.util.Objects;
+
+public class Templar extends Character implements Healer, Tank {
+    private final int healCapacity;
+    private final int shield;
+
+    public Templar(String name, int max, int heal, int shield, Weapon w) {
+        super(name, max, w);
+        this.healCapacity = heal;
+        this.shield = shield;
+
+    }
+
+    public int getHealCapacity() {
+        return this.healCapacity;
+    }
+
+    @Override
+    public void attack(Character chart) {
+        if (Objects.nonNull(this.getWeapon())) {
+            heal(this);
+            chart.takeDamage(this.getWeapon().getDamage());
+        } else {
+            heal(this);
+            chart.takeDamage(6);
+        }
+    }
+
+    @Override
+    public void takeDamage(int nb) {
+        if (this.getCurrentHealth() - (nb - this.shield) >= 0) {
+            this.setCurrentHealth(this.getCurrentHealth() - (nb - this.shield));
+        } else {
+            this.setCurrentHealth(0);
+        }
+
+    }
+
+    public void heal(Character cha) {
+        if (cha.getCurrentHealth() + this.healCapacity > cha.getMaxHealth()) {
+            cha.setCurrentHealth(cha.getMaxHealth());
+        } else {
+
+            cha.setCurrentHealth(cha.getCurrentHealth() + this.healCapacity);
+        }
+    }
+
+    public int getShield() {
+        return this.shield;
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() != 0) {
+            return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d. He has the weapon %s",
+                    this.getName(), this.getCurrentHealth(),
+                    this.healCapacity, this.shield, this.getWeapon().toString());
+        } else {
+            return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %d HP. He has the weapon %s",
+                    this.getName(),
+                    this.shield, this.healCapacity, this.getWeapon().toString());
+        }
+
+    }
+
+}
