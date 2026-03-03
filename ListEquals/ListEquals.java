7dc833270da895158b637623364b04e8b9e8283d
RedaAz07 <annizreda07@gmail.com>
2026-03-03 11:39:41 +0000

    fix equals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,11 +2,15 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-       return  list1.equals(list2);
+
+        return list1 != null && list2 != null && list1.equals(list2);
         // your code here
     }
-     public static void main(String[] args) {
-        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Charly", "Emily")));
-        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"), List.of("Alice", "Bob", "Emily", "Charly")));
+
+    public static void main(String[] args) {
+        System.out.println(ListEquals.areListsEqual(null,
+                List.of("Alice", "Bob", "Charly", "Emily")));
+        System.out.println(ListEquals.areListsEqual(List.of("Alice", "Bob", "Charly", "Emily"),
+                List.of("Alice", "Bob", "Emily", "Charly")));
     }
 }
