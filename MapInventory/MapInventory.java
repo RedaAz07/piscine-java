f0edd57eb71aff4cb8dab484c8e64a2985ef14d9
RedaAz07 <annizreda07@gmail.com>
2026-03-03 12:28:07 +0000

    d

---
+++ MapInventory/MapInventory.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapInventory {
+    public static int getProductPrice(Map<String, Integer> inventory, String productId) {
+        // your code here
+    }
+
+    public static List<String> getProductIdsByPrice(Map<String, Integer> inventory, int price) {
+        // your code here
+    }
+}
